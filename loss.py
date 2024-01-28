35ca7384bec985b15882e28008fc90f04a7c8639
Aniket Patil <128228805+AniketP04@users.noreply.github.com>
2024-01-28 21:31:57 +0530

    Update loss.py

--- loss.py
+++ loss.py
@@ -3,8 +3,16 @@ import torch
 def pointnetloss(outputs, labels, m3x3, m64x64, alpha = 0.0001):
     criterion = torch.nn.NLLLoss()
     batchsize = outputs.size(0)
-    I = torch.eye(64, requires_grad=True).repeat(batchsize,1,1)
+    
+    id3x3 = torch.eye(3, requires_grad=True).repeat(batchsize, 1, 1)
+    id64x64 = torch.eye(64, requires_grad=True).repeat(batchsize, 1, 1)
+   
     if outputs.is_cuda:
-        I = I.cuda()
-    loss = criterion(outputs, labels) + alpha * torch.mean(torch.norm(I - torch.bmm(m64x64, m64x64.transpose(1,2)), dim=(1,2))) 
+        id3x3 = id3x3.cuda()
+        id64x64 = id64x64.cuda()
+
+    diff3x3 = id3x3-torch.bmm(m3x3, m3x3.transpose(1,2))
+    diff64x64 = id64x64-torch.bmm(m64x64, m64x64.transpose(1, 2))
+    
+    loss = criterion(ouputs, labels) + alpha * (torch.norm(diff3x3) + torch.norm(diff64x64))/float(batchsize) 
     return loss

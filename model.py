c2765f9b34761ac5ceb40031833f8b9c432df528
Aniket Patil <128228805+AniketP04@users.noreply.github.com>
2024-01-28 21:57:03 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,100 +2,105 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Tnet(nn.Module):
-    def __init__(self, k=3):
-        super(Tnet, self).__init__()
-        self.k = k 
-        self.conv1 = nn.Conv1d(k, 64, 1)
-        self.conv2 = nn.Conv1d(64, 128, 1)
-        self.conv3 = nn.Conv1d(128, 1024, 1)
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256) 
-        self.fc3 = nn.Linear(256, k*k)
+class MLP_CONV(nn.Module):
+   def __init__(self, input_size, output_size):
+     super().__init__()
+     self.input_size   = input_size
+     self.output_size  = output_size
+     self.conv  = nn.Conv1d(self.input_size, self.output_size, 1)
+     self.bn    = nn.BatchNorm1d(self.output_size)
 
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(128)
-        self.bn3 = nn.BatchNorm1d(1024)
-        self.bn4 = nn.BatchNorm1d(512)
-        self.bn5 = nn.BatchNorm1d(256)
+   def forward(self, input):
+     return F.relu(self.bn(self.conv(input)))
 
-    def forward(self, x):
-        batchsize = x.size(0)
-        n_points = x.size(2)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = nn.MaxPool1d(kernel_size=n_points)(x)
-        x = nn.Flatten(start_dim=1)(x) 
-        x = F.relu(self.bn4(self.fc1(x)))
-        x = F.relu(self.bn5(self.fc2(x)))
+class FC_BN(nn.Module):
+   def __init__(self, input_size, output_size):
+     super().__init__()
+     self.input_size   = input_size
+     self.output_size  = output_size
+     self.lin  = nn.Linear(self.input_size, self.output_size)
+     self.bn    = nn.BatchNorm1d(self.output_size)
 
-        init = torch.eye(self.k, requires_grad=True).repeat(batchsize,1,1) 
-        if x.is_cuda: 
-            init = init.cuda()
-        matrix = self.fc3(x).view(-1,self.k,self.k) + init 
-        return matrix
+   def forward(self, input):
+     return F.relu(self.bn(self.lin(input)))
 
+class TNet(nn.Module):
+   def __init__(self, k=3):
+      super().__init__()
+      self.k=k
+
+      self.mlp1 = MLP_CONV(self.k, 64)
+      self.mlp2 = MLP_CONV(64, 128)
+      self.mlp3 = MLP_CONV(128, 1024)
+
+      self.fc_bn1 = FC_BN(1024, 512)
+      self.fc_bn2 = FC_BN(512,256)
+
+      self.fc3 = nn.Linear(256,k*k)
+
+
+   def forward(self, input):
+      # input.shape == (batch_size,n,3)
+
+      bs = input.size(0)
+      xb = self.mlp1(input)
+      xb = self.mlp2(xb)
+      xb = self.mlp3(xb)
+
+      pool = nn.MaxPool1d(xb.size(-1))(xb)
+      flat = nn.Flatten(1)(pool)
+
+      xb = self.fc_bn1(flat)
+      xb = self.fc_bn2(xb)
+
+      init = torch.eye(self.k, requires_grad=True).repeat(bs,1,1)
+      if xb.is_cuda:
+        init=init.cuda()
+      matrix = self.fc3(xb).view(-1,self.k,self.k) + init
+      return matrix
     
-class Transform(nn.Module):
-    def __init__(self, global_feature=True):
-        super(Transform, self).__init__()
-        self.input_transform = Tnet(k=3)
-        self.feature_transform = Tnet(k=64)
-
-        self.conv1 = nn.Conv1d(3, 64, 1)
-        self.conv2 = nn.Conv1d(64, 128, 1)
-        self.conv3 = nn.Conv1d(128, 1024, 1)
-
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(128)
-        self.bn3 = nn.BatchNorm1d(1024)
-
-        self.global_feature = global_feature 
-
-    def forward(self, x):
-        n_points = x.size(2)
-
-        m3x3 = self.input_transform(x)
-        x = torch.bmm(torch.transpose(x,1,2), m3x3).transpose(1,2) # batch matrix multiplication
-        x = F.relu(self.bn1(self.conv1(x)))
-
-        m64x64 = self.feature_transform(x)
-        x = torch.bmm(torch.transpose(x,1,2), m64x64).transpose(1,2) # batch matrix multiplication
-        localfeat = x # extract local features for segmentation task 
-
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = self.bn3(self.conv3(x))
-        x = nn.MaxPool1d(kernel_size=x.size(-1))(x)
-        out = nn.Flatten(start_dim=1)(x)
+class PointNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_transform = TNet(k=3)
+        self.feature_transform = TNet(k=64)
+        self.mlp1 = MLP_CONV(3, 64)
+        self.mlp2 = MLP_CONV(64, 128)
         
-        if self.global_feature: 
-            return out, m3x3, m64x64
-        else: 
-            out = out.view(-1,1024,1).repeat(1,1,n_points)
-            return torch.cat([localfeat, out], dim=1), m3x3, m64x64
+        self.conv = nn.Conv1d(128, 1024, 1)
+        
+        self.bn = nn.BatchNorm1d(1024)
+
+    def forward(self, input):
+        n_pts = input.size()[2]
+        matrix3x3 = self.input_transform(input)
+        input_transform_output = torch.bmm(torch.transpose(input,1,2), matrix3x3).transpose(1,2)
+        x = self.mlp1(input_transform_output)
+        matrix64x64 = self.feature_transform(x)
+        feature_transform_output =  torch.bmm(torch.transpose(x,1,2), matrix64x64).transpose(1,2)
+        x = self.mlp2(feature_transform_output)
+        x = self.bn(self.conv(x))
+        global_feature = nn.MaxPool1d(x.size(-1))(x)
+        global_feature_repeated = nn.Flatten(1)(global_feature).repeat(n_pts,1,1).transpose(0,2).transpose(0,1)
+
+        return [feature_transform_output, global_feature_repeated], matrix3x3, matrix64x64
 
 
-class PointNetSegmentation(nn.Module):
-    def __init__(self, num_classes=10):
-        super(PointNetSegmentation, self).__init__()
-        self.num_classes = num_classes
-        self.transform = Transform(global_feature=False)
-        self.conv1 = nn.Conv1d(1088, 512, 1)
-        self.conv2 = nn.Conv1d(512, 256, 1)
-        self.conv3 = nn.Conv1d(256, 128, 1)
-        self.conv4 = nn.Conv1d(128, num_classes, 1)
-
-        self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.bn3 = nn.BatchNorm1d(128)
-
-    def forward(self, x):
-        x, m3x3, m64x64 = self.transform(x)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = self.conv4(x)
-        x = F.log_softmax(x, dim=1)
-        x = x.transpose(1,2)
-        return x, m3x3, m64x64
+class PointNetSeg(nn.Module):
+    def __init__(self, classes=3):
+        super().__init__()
+        self.pointnet = PointNet()
+        self.mlp1 = MLP_CONV(1088, 512)
+        self.mlp2 = MLP_CONV(512, 256)
+        self.mlp3 = MLP_CONV(256, 128)
+        self.conv = nn.Conv1d(128, classes, 1)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+    
+    def forward(self, input):
+        inputs, matrix3x3, matrix64x64 = self.pointnet(input)
+        stack = torch.cat(inputs,1)
+        x = self.mlp1(stack)
+        x = self.mlp2(x)
+        x = self.mlp3(x)
+        output = self.conv(x)
+        return self.logsoftmax(output), matrix3x3, matrix64x64
